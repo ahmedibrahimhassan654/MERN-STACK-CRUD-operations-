c9075c07e30a12a6c73dfdeaa75a5b328865718d
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-02-26 14:42:48 +0200

    change server port

--- server/index.js
+++ server/index.js
@@ -7,8 +7,8 @@ var postMessageRoutes = require("./controllers/postMessageController");
 
 var app = express();
 app.use(bodyParser.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "http://localhost:4000" }));
 
 app.use("/postMessages", postMessageRoutes);
 
-app.listen(3000, () => console.log("Server started at : 3000"));
+app.listen(4000, () => console.log("Server started at : 4000"));
