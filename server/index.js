9a9600a3c2833d2e6443ad10c4201d66a2047e84
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-02-27 19:00:18 +0200

    create react app

--- server/index.js
+++ server/index.js
@@ -9,6 +9,5 @@ var app = express();
 app.use(bodyParser.json());
 app.use(cors({ origin: "http://localhost:3000" }));
 
-app.use("/postMessages", postMessageRoutes);
-
 app.listen(4000, () => console.log("Server started at : 4000"));
+app.use("/postMessages", postMessageRoutes);
